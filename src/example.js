e0806d2d3b33245fb8fbb7ab0d29ffaf66d037b9
Paulo Ragonha <paulo@ragonha.me>
2017-09-23 17:52:43 +0200

    Cleanup 🗑

--- src/example.js
+++
@@ -1,9 +0,0 @@
-const most = require('most')
-
-const infinite500 = most.periodic(500)
-
-const fixed = most.from([1, 2, 3]).flatMap(value => infinite500.map(() => value))
-
-fixed.subscribe({
-  next: value => console.log(value),
-})
